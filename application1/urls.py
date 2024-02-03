3a816c15b3363b991844d4940b7c90f5bf4147ac
Czaja <dczajka1@gmail.com>
2024-02-03 17:53:45 +0100

    deletion for practice. will start again

--- application1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_view),
-    path('<str:topic>', views.news_view),
-    path('<int:num_page>', views.num_page_view)
-]
